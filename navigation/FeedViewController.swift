f8f2d3763a9c28fc7260df3292f93e8a83537bc0
LozovoyKirill <122818728+LozovoyKirill@users.noreply.github.com>
2023-04-12 17:04:41 +0300

    create FeedViewController

---
+++ navigation/FeedViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FeedViewController.swift
+//  navigation
+//
+//  Created by Кирилл Левинсон on 12.04.2023.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "News feed"
+        view.backgroundColor = UIColor.orange
+        let postButton = UIBarButtonItem(title: "Post", style: .plain, target: self, action: #selector(didTapPost))
+        navigationItem.rightBarButtonItem = postButton
+    }
+    @objc private func didTapPost() {
+        let post = Post(title: "New post")
+        let postVC = PostViewController(nibName: nil, bundle: nil)
+        navigationController?.pushViewController(postVC, animated: true)
+    }
+}
