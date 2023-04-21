27e2a367ea0f24d8a1351a872debda1affcf50f0
LozovoyKirill <122818728+LozovoyKirill@users.noreply.github.com>
2023-04-21 11:12:59 +0300

    edit postviewcontroller

--- navigation/PostViewController.swift
+++ navigation/PostViewController.swift
@@ -7,19 +7,21 @@
 
 import UIKit
 
-struct Post {
-    let title: String
-}
 
 class PostViewController: UIViewController {
     var post: Post?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.green
+        infoButton()
         navigationItem.title = post?.title
+    }
+    private func infoButton(){
         let infoButton = UIBarButtonItem(title: "Info", style: .plain, target: self, action: #selector(didTapInfo))
         navigationItem.rightBarButtonItem = infoButton
-        view.backgroundColor = UIColor.green
     }
+    
     @objc private func didTapInfo() {
         let infoVC = InfoViewController()
         present(infoVC, animated: true, completion: nil)
