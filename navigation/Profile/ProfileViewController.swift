024f5157e09ce73963a0b3a8cb5d699acf6976dd
LozovoyKirill <122818728+LozovoyKirill@users.noreply.github.com>
2023-04-24 17:35:42 +0300

    edit

--- navigation/Profile/ProfileViewController.swift
+++ navigation/Profile/ProfileViewController.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    let profileHeaderView = ProfileHeaderView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.orange
+        
+        view.backgroundColor = .lightGray
         navigationItem.title = "Profile"
+        view.addSubview(profileHeaderView)
+        
+        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+               NSLayoutConstraint.activate([
+                   profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                   profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                   profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+               ])
+               profileHeaderView.frame = view.frame
     }
 }
