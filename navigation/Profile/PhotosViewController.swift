96af56e8b1bb4b5c7a6f3957d0e4a9b0efc65aac
LozovoyKirill <122818728+LozovoyKirill@users.noreply.github.com>
2023-05-30 10:29:23 +0300

    create collection

--- navigation/Profile/PhotosViewController.swift
+++ navigation/Profile/PhotosViewController.swift
@@ -5,4 +5,81 @@
 //  Created by Кирилл Левинсон on 24.05.2023.
 //
 
-import Foundation
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    private let addImages = PhotoPost.makeArray()
+    
+    private lazy var photocollectionView: UICollectionView = {
+        //создание стандартного лейаут
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemGray2
+        collectionView.register(PhotosCollectionViewCell.self, forCellWithReuseIdentifier: PhotosCollectionViewCell.identifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubviews()
+        setConstraits()
+        navigationController?.navigationBar.isHidden = false
+        title = "Photo Gallery"
+        photocollectionView.reloadData()
+    }
+    
+    
+    private func addSubviews(){
+        view.addSubview(photocollectionView)
+    }
+    
+    private func setConstraits() {
+        NSLayoutConstraint.activate([
+            photocollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            photocollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            photocollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            photocollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+}
+
+
+
+//MARK: UICollectionViewDataSource
+
+extension PhotosViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        addImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotosCollectionViewCell.identifier, for: indexPath) as! PhotosCollectionViewCell
+        cell.setupCell(model: addImages[indexPath.item])
+        return cell
+    }
+}
+
+extension PhotosViewController: UICollectionViewDelegateFlowLayout {
+    private var inset: CGFloat { return 8}
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.bounds.width - inset * 4) / 3
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        inset
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        inset
+    }
+}
