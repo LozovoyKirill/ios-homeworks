24ad9c72d614d9b8a7afc58b220fb57c42600a1e
LozovoyKirill <122818728+LozovoyKirill@users.noreply.github.com>
2023-06-26 16:00:18 +0300

    add viewcontroller

--- navigation/Profile/PostModalViewController.swift
+++ navigation/Profile/PostModalViewController.swift
@@ -4,5 +4,103 @@
 //
 //  Created by Кирилл Левинсон on 24.06.2023.
 //
+import UIKit
 
-import Foundation
+class PostModalViewController: UIViewController {
+    
+    private let authorLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 26)
+        return label
+    }()
+    
+    private let postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.numberOfLines = 0
+        label.textColor = UIColor.systemGray
+        return label
+    }()
+    
+    private let likes: UILabel = {
+        let label = UILabel()
+        label.isUserInteractionEnabled = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20)
+        return label
+    }()
+    
+    private let views: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20)
+        return label
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .systemGray6
+        stackView.addArrangedSubview(views)
+        stackView.addArrangedSubview(likes)
+        view.addSubview(authorLabel)
+        view.addSubview(postImageView)
+        view.addSubview(descriptionLabel)
+        view.addSubview(stackView)
+    }
+    
+    func setupData(model: Post) {
+        authorLabel.text = model.author
+        postImageView.image = UIImage(named: model.image)
+        descriptionLabel.text = model.description
+        likes.text = "Likes: \(model.likes)"
+        views.text = "Views: \(model.views)"
+    }
+}
+extension PostModalViewController {
+    func setupConstraints() {
+        let screenRect = UIScreen.main.bounds
+        let screenWidth = screenRect.size.width
+        
+        NSLayoutConstraint.activate([
+            
+            authorLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            authorLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            authorLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 16),
+            
+            postImageView.topAnchor.constraint(equalTo: authorLabel.bottomAnchor, constant: 16),
+            postImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            postImageView.widthAnchor.constraint(equalToConstant: screenWidth),
+            postImageView.heightAnchor.constraint(equalToConstant: screenWidth),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: postImageView.bottomAnchor, constant: 16),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            stackView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+}
